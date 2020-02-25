88a33da86d7b6c2cbb921f252d668df7463efdb9
Ashish <asaschoudhary616@gmail.com>
2020-02-25 12:53:35 +0530

    ADDED: Display employee is present or not

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 #printing
-echo "Welcome to Employee Wage Computation"
+echo "We are printing employee is present or not"
+isPresent=$(($RANDOM%2))
+if((isPresent == 1))
+then
+	echo "Employee is present"
+else
+	echo "Employee is not present"
+fi
+
+
+
