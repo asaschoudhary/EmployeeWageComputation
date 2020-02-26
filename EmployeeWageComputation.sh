17d45cc2f1791eea71e8e41fb5d70fd17d087e6f
Ashish <asaschoudhary616@gmail.com>
2020-02-26 11:51:51 +0530

    MODIFIED: Daily wages and Total wages

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,30 +1,56 @@
 #!/bin/bash -x
-isFullTime=2;
-isPartTime=1;
-maxHourInMonth=10
-empRatePerHour=20
-numWorkingDay=20
-empRatePerHour=20;
-#numberWorkingDay=20
-#Varival
-totalEmpHour=0
-totalWorkingDay=0
-while [[ $totalEmpHour -lt $maxHourInMonth &&
-		$totalWokkingDay -lt $numWorkingDay ]]
+printf "Added function to calculate daily wages and store it in array"
+
+#CONSTANTS
+WAGEPERHOUR=20
+WORKINGDAYS=20
+TOTALWORKINGHOURS=200
+
+#VARIABLES
+hoursPerDay=0
+isPartTime=1
+isPresent=2
+wage=0
+monthlyWage=0
+day=1
+totalHoursWorked=0
+declare -a empDailyWage
+
+#FUNCTION TO GENERATE WORKING HOURS PER DAY
+function getWorkingHours()
+{
+	random=$1
+	case $random in 
+      $isPresent)
+         hoursPerDay=8
+         ;;
+      $isPartTime)
+         hoursPerDay=4
+         ;;
+      *)
+         hoursPerDay=0
+         ;;
+	esac
+	echo $hoursPerDay
+}
+
+#CALCULATING DAILY WAGES
+function calculateDailyWage()
+{
+	local workHours=$1
+	wage=$((workHours*WAGEPERHOUR))
+	echo $wage
+}
+
+#CALCULATING MONTHLY WAGES AND TOTAL WORKING HOURS IN WHILE LOOP TILL ANY OF THEM IS REACHED
+while(($day<=$WORKINGDAYS && $totalHoursWorked<=$TOTALWORKINGHOURS)) 
 do
-	((totalWorkingDay++))
-	random=$((RANDOM%3));
-case  $random in 
-	$isFullTime)
- 	empHour=8
-	;;
-	$isPartTime)
-	empHour=4
-	;;
-	*)
-	empHour=0
-	;;
-esac
-	totalEmpHour=$(($totalEmpHour+$empHour))
-done 
-salary=$(($totalEmpHour*$empRatePerHour))
+	random=$((RANDOM%3))
+	hoursPerDay="$( getWorkingHours $random )"
+	totalHoursWorked=$((totalHoursWorked+hoursPerDay))
+	empDailyWage[$day]="$( calculateDailyWage  $hoursPerDay)"
+	day=$((day+1))
+done
+monthlyWage="$( calculateDailyWage  $totalHoursWorked)"
+printf "Month wage: $monthlyWage\n"
+printf "Daily wage: " ${empDailyWage[@]}
